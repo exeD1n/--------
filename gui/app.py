e95aa846d9050e2edee501b9ae8c676340228ea6
Lutaev D.0. <daniillutaev1@gmail.com>
2023-12-25 19:24:14 +0400

    asd

--- gui/app.py
+++ gui/app.py
@@ -3,16 +3,14 @@ import os
 from pathlib import Path
 import platform
 import subprocess
+import time
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import simpledialog
 from tkinter import filedialog
 from tkinter import ttk
+import psutil
 from ttkthemes import ThemedTk
-from gui.performance_window import PerformanceWindow
-from gui.processes_window import ProcessesWindow
-from gui.terminal_gui import TerminalGUI
-from gui.channel_info_app import ChannelInfoApp
 
 class App:
     def __init__(self, root):
@@ -24,10 +22,6 @@ class App:
 
         self.performance_window = None
         self.processes_window = None
-        
-        
-        self.file_system_window = None  # Добавьте эту строку
-
 
         container = tk.Frame(root)
         container.pack(fill="both", expand=True)
@@ -42,12 +36,7 @@ class App:
         self.root.config(menu=mainmenu)
 
         utilities_menu = tk.Menu(mainmenu, tearoff=0)
-        utilities_menu.add_command(label="Открыть файловый менеджер", command=self.open_file_manager)
-       
-       
-        utilities_menu.add_command(label="Открыть системный менеджер", command=self.open_system_manager)
-       
-       
+        utilities_menu.add_command(label="Открыть файловый менеджер", command=self.open_file_manager)       
         utilities_menu.add_command(label="Выполнить поиск", command=self.execute_search)
         utilities_menu.add_command(label="О приложении", command=self.show_about)
         utilities_menu.add_separator()
@@ -60,66 +49,6 @@ class App:
         button2.pack(pady=10, fill="both", expand=True)
         button3.pack(pady=10, fill="both", expand=True)
         button4.pack(pady=10, fill="both", expand=True)
-
-
-
-
-
-    def open_system_manager(self):
-        if self.file_system_window:
-            self.file_system_window.destroy()
-
-        # Создадим новое окно для отображения файловой системы
-        self.file_system_window = tk.Toplevel(self.root)
-        self.file_system_window.title("Файловый Менеджер")
-        self.file_system_window.geometry("600x400")
-
-        # Treeview для отображения файловой системы
-        self.file_system_tree = ttk.Treeview(self.file_system_window)
-        self.file_system_tree.pack(side="left", fill="both", expand=True)
-        self.file_system_tree.heading("#0", text="Файловая Система", anchor="w")
-        self.file_system_tree.bind("<Double-1>", self.on_file_system_tree_double_click)
-
-        # Полоса прокрутки для Treeview
-        tree_scroll = ttk.Scrollbar(self.file_system_window, orient="vertical", command=self.file_system_tree.yview)
-        tree_scroll.pack(side="right", fill="y")
-        self.file_system_tree.configure(yscrollcommand=tree_scroll.set)
-
-        # Получим корневой каталог в зависимости от операционной системы
-        root_dir = "/" if os.name == "posix" else "C:/"
-        self.populate_file_system_tree(root_dir)
-
-    def populate_file_system_tree(self, directory, parent_item=""):
-        try:
-            for item in os.scandir(directory):
-                try:
-                    item_path = item.path.replace("/", os.path.sep)  # Используем правильный разделитель путей
-                    is_directory = item.is_dir()
-                    open_state = "" if is_directory else "open"  # Изменили здесь
-                    item_id = self.file_system_tree.insert(parent_item, "end", text=item.name, open=open_state)
-                    if is_directory:
-                        # Если элемент является директорией, заполним ее содержимым
-                        self.populate_file_system_tree(item_path, item_id)
-                    else:
-                        # Если элемент - файл, добавим его в дерево с указанием расширения
-                        self.file_system_tree.insert(item_id, "end", text=f"{item.name} ({item.name.split('.')[-1] if '.' in item.name else ''})")
-                except (PermissionError, FileNotFoundError) as e:
-                    # Игнорируем ошибки доступа и отсутствия файла
-                    print(f"Пропущен элемент {item.path}: {e}")
-        except Exception as e:
-            print(f"Ошибка при заполнении каталога {directory}: {e}")
-
-    def on_file_system_tree_double_click(self, event):
-        selected_item = self.file_system_tree.selection()
-        if selected_item:
-            item_path = os.path.join(*selected_item).replace("/", os.path.sep)
-            if os.path.isdir(item_path):
-                self.populate_file_system_tree(item_path, selected_item)
-
-
-
-
-
 
     def show_performance(self):
         self.root.withdraw()
@@ -208,3 +137,224 @@ class App:
             messagebox.showinfo("Поиск завершен", result_text)
         else:
             messagebox.showinfo("Поиск завершен", "Ничего не найдено.")
+            
+            
+class ChannelInfoApp:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Информация о каналах в ОС")
+
+        self.processes_info = self.get_os_channels()
+
+        label = tk.Label(self.master, text="Информация о каналах в операционной системе:")
+        label.pack()
+
+        self.canvas = tk.Canvas(self.master)
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        self.scrollbar = tk.Scrollbar(self.master, command=self.canvas.yview)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        self.canvas.bind('<Configure>', lambda e: self.canvas.configure(scrollregion=self.canvas.bbox('all')))
+
+        self.frame = tk.Frame(self.canvas)
+        self.canvas.create_window((0, 0), window=self.frame, anchor='nw')
+
+        for process in self.processes_info:
+            button_text = f"PID: {process['pid']}, Имя: {process['name']}"
+            button = tk.Button(self.frame, text=button_text, command=lambda info=process['pid']: self.show_connection_info(info))
+            button.pack()
+
+        # Кнопка для возвращения в главное меню
+        return_button = tk.Button(self.master, text="Вернуться в главное меню", command=self.return_to_main_menu)
+        return_button.pack(pady=10, fill="both", expand=True)
+
+    def get_os_channels(self):
+        # Получаем информацию о процессах и их каналах через psutil
+        processes_info = []
+        for process in psutil.process_iter(['pid', 'name', 'connections']):
+            process_info = {
+                'pid': process.info['pid'],
+                'name': process.info['name'],
+                'connections': process.info['connections']
+            }
+            if process_info['connections']:
+                processes_info.append(process_info)
+        return processes_info
+
+    def show_connection_info(self, pid):
+        # Дополнительная информация о каналах для выбранного процесса
+        connection_info = [conn for process in self.processes_info if process['pid'] == pid for conn in process['connections']]
+        self.show_connection_window(connection_info)
+
+    def show_connection_window(self, connection_info):
+        # Отображаем окно с информацией о каналах
+        connection_window = tk.Toplevel(self.master)
+        connection_window.title("Дополнительная информация о каналах")
+
+        for connection in connection_info:
+            label_text = f"Тип: {connection.type}, Локальный адрес: {connection.laddr}, Удаленный адрес: {connection.raddr}"
+            label = tk.Label(connection_window, text=label_text)
+            label.pack()
+
+    def return_to_main_menu(self):
+        self.master.destroy()
+        
+# gui/terminal_gui.py
+import tkinter as tk
+from tkinter import scrolledtext
+import subprocess
+import platform
+
+class TerminalGUI:
+    def __init__(self, master, return_to_main_menu_callback):
+        self.master = master
+        self.return_to_main_menu_callback = return_to_main_menu_callback
+        master.title("Графический терминал")
+
+        # Создаем текстовое поле для вывода результатов
+        self.output_text = scrolledtext.ScrolledText(master, wrap=tk.WORD, width=50, height=20)
+        self.output_text.pack(expand=True, fill=tk.BOTH)
+
+        # Создаем поле для ввода команд
+        self.input_entry = tk.Entry(master, width=50)
+        self.input_entry.pack(expand=True, fill=tk.BOTH)
+
+        # Создаем кнопку для выполнения команды
+        self.run_button = tk.Button(master, text="Выполнить", command=self.execute_command)
+        self.run_button.pack(expand=True, fill=tk.BOTH)
+
+        # Создаем кнопку для возврата в главное меню
+        self.return_button = tk.Button(master, text="Вернуться в главное меню", command=self.return_to_main_menu)
+        self.return_button.pack(expand=True, fill=tk.BOTH)
+
+    def execute_command(self):
+        # Получаем команду из поля ввода
+        command = self.input_entry.get()
+
+        try:
+            # Проверяем платформу для корректного запуска команд
+            if platform.system() == "Windows":
+                process = subprocess.Popen(
+                    command, shell=True, stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE, stdin=subprocess.PIPE, text=True
+                )
+            else:
+                # For Linux, split the command into a list for proper execution
+                process = subprocess.Popen(
+                    command.split(), shell=False, stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE, stdin=subprocess.PIPE, text=True
+                )
+
+            # Получаем результат выполнения команды
+            output, error = process.communicate()
+
+            # Очищаем текстовое поле вывода
+            self.output_text.delete(1.0, tk.END)
+
+            # Выводим результат в текстовое поле
+            self.output_text.insert(tk.END, output)
+            self.output_text.insert(tk.END, error)
+
+        except Exception as e:
+            # Handle exceptions and display the error message
+            self.output_text.delete(1.0, tk.END)
+            self.output_text.insert(tk.END, f"Error: {str(e)}")
+
+    def return_to_main_menu(self):
+        # Закрываем окно терминала
+        self.master.destroy()
+
+        # Возвращаемся в главное меню, вызывая колбэк
+        if self.return_to_main_menu_callback:
+            self.return_to_main_menu_callback()
+            
+class PerformanceWindow:
+    def __init__(self, master, on_close_callback):
+        self.master = master
+        self.on_close_callback = on_close_callback
+
+        self.performance_window = tk.Toplevel(master)
+        self.performance_window.title("Производительность ОС")
+        self.performance_window.geometry("400x200")
+
+        self.label_memory = tk.Label(self.performance_window, text="Используемая виртуальная память:")
+        self.label_cpu = tk.Label(self.performance_window, text="Общая загрузка процессора:")
+        self.label_available_ram = tk.Label(self.performance_window, text="Доступная ОЗУ:")
+        self.label_total_ram = tk.Label(self.performance_window, text="Всего ОЗУ:")
+
+        self.label_memory.pack()
+        self.label_cpu.pack()
+        self.label_available_ram.pack()
+        self.label_total_ram.pack()
+
+        self.update_performance_data()
+
+        # Кнопка для возвращения в главное меню
+        self.return_button = tk.Button(self.performance_window, text="Вернуться в главное меню", command=self.return_to_main_menu)
+        self.return_button.pack(pady=10)
+
+    def update_performance_data(self):
+        memory_info = psutil.virtual_memory()
+        cpu_percent = psutil.cpu_percent(interval=1)
+        
+        self.label_memory.config(text=f"Используемая виртуальная память: {memory_info.used / (1024 ** 3):.2f} GB")
+        self.label_cpu.config(text=f"Общая загрузка процессора: {cpu_percent}%")
+        self.label_available_ram.config(text=f"Доступная ОЗУ: {memory_info.available / (1024 ** 3):.2f} GB")
+        self.label_total_ram.config(text=f"Всего ОЗУ: {memory_info.total / (1024 ** 3):.2f} GB")
+
+        # Планируем обновление данных через 1 секунду
+        self.performance_window.after(1000, self.update_performance_data)
+
+    def return_to_main_menu(self):
+        self.on_close_callback()
+
+    def destroy(self):
+        self.performance_window.destroy()
+
+class ProcessesWindow:
+    def __init__(self, master, on_close_callback):
+        self.master = master
+        self.on_close_callback = on_close_callback
+
+        self.processes_window = tk.Toplevel(master)
+        self.processes_window.title("Просмотр процессов")
+        self.processes_window.geometry("500x300")
+
+        self.process_listbox = tk.Listbox(self.processes_window, selectmode=tk.SINGLE, exportselection=False)
+        self.process_listbox.pack(expand=True, fill=tk.BOTH)
+
+        self.process_listbox.bind("<Double-Button-1>", self.on_double_click)
+
+        # Кнопка для обновления данных
+        self.refresh_button = tk.Button(self.processes_window, text="Обновить", command=self.update_processes_data)
+        self.refresh_button.pack(pady=10)
+
+        # Кнопка для возвращения в главное меню
+        self.return_button = tk.Button(self.processes_window, text="Вернуться в главное меню", command=self.return_to_main_menu)
+        self.return_button.pack(pady=10)
+
+        self.update_processes_data()
+
+    def update_processes_data(self):
+        self.process_listbox.delete(0, tk.END)
+
+        for process in psutil.process_iter(['pid', 'name', 'create_time']):
+            if process.info['name'] != 'python.exe' and process.info['pid'] != os.getpid():
+                process_name = process.info['name']
+                create_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(process.info['create_time']))
+                item_text = f"{process_name} (PID: {process.info['pid']}, Запущен: {create_time})"
+                self.process_listbox.insert(tk.END, item_text)
+
+    def on_double_click(self, event):
+        selected_index = self.process_listbox.curselection()
+        if selected_index:
+            selected_process_info = self.process_listbox.get(selected_index)
+            messagebox.showinfo("Детали процесса", selected_process_info)
+
+    def return_to_main_menu(self):
+        self.on_close_callback()
+
+    def destroy(self):
+        self.processes_window.destroy()
