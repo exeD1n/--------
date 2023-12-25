9ed988554917ec1d33eb3871694f08919cf66807
Lutaev D.0. <daniillutaev1@gmail.com>
2023-12-25 18:59:24 +0400

    ыва

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 # main.py
-import os
 from gui.app import App
 from ttkthemes import ThemedTk
 
 if __name__ == "__main__":
-    if os.geteuid() != 0:
-        # Если не является, запускаем программу с sudo
-        os.system("sudo python3 main.py")
-    else:
-        # Если уже root, создаем ThemedTk и запускаем программу
-        root = ThemedTk(theme="arc")
-        app = App(root)
-        root.mainloop()
+    root = ThemedTk(theme="arc")
+    app = App(root)
+    root.mainloop()
